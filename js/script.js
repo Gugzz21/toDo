88bbfa4a98ef4da88e8eed822eafd120e84a4620
Gugzz21 <gustavodinizz966@gmail.com>
2025-08-12 09:56:12 -0300

    Terceiro commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+const criarTarefa = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    const valor = input.value; 
+    const lista = document.querySelector("[data-task]");
+
+    const tarefa = document.createElement("li");
+    tarefa.classList.add("task");
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo; 
+    lista.appendChild(tarefa);
+    
+    
+    input.value = "";
+};
+
 const novaTarefa = document.querySelector("[data-form-button]");
+novaTarefa.addEventListener('click', criarTarefa);
 
-
-novaTarefa.addEventListener('click', () => {
-    const tarefa = document.querySelector("[data-form-input]");
-    const valor = tarefa.value; 
-    console.log(valor);
-});
